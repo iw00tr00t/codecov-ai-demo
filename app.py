ce083f4eb3f0c37a8f15014a886ab81a810329d2
Avinash <avinash.thapa@icloud.com>
2025-06-05 00:31:06 -0400

    Initial dummy vulnerable app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/ssrf')
+def ssrf():
+    url = request.args.get('url')
+    return requests.get(url).text
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    f = request.files['file']
+    f.save('/tmp/' + f.filename)
+    return "Uploaded"
